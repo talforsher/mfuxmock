91c5c84a4ffeb4fefdb1da88c03b7638cc699bf2
talforsher1@gmail.com <talforsher1@gmail.com>
2020-08-11 20:23:28 +0300

    first

--- components.js
+++ components.js
@@ -42,7 +42,7 @@ EL('main-app',
                     </div>
                     <div class="intro">intro</div>
                     <div class="content">
-                    <div>content</div>
+                    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FhVKDlMRrCmwWQXUcVjDTJK%2FButtons-DS.2020%3Fnode-id%3D1%253A2&chrome=DOCUMENTATION" allowfullscreen></iframe>
                     </div>
                 </div>
                 </div>
